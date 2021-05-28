8fca7b37f0ac9d741cfab6a47d9d1bf8899d176a
Aditya Prakash <aditya.eth@gmail.com>
2021-05-28 21:38:24 +0530

    comments for hellp

--- libs.sh
+++ libs.sh
@@ -5,11 +5,11 @@ export scripts
 
 check_sheets() {
     echo "Checking Sheets.."
-    questions=`cat $answer | grep -i "." | wc -l`
+    questions=`cat $answer | grep -i "." | wc -l` #suggest alternate
     echo "Number of questions found: $questions"
     echo 
     for i in $scripts; do
-        echo $i `diff $answer $i | grep -i ">" | wc -l`>> marks.txt
+        echo $i `diff $answer $i | grep -i ">" | wc -l`>> marks.txt # how to store $questions-(no. of difference)
     done
     sed -i -e 's/\(answer_scripts[/]\|txt\)//g' marks.txt
 }
